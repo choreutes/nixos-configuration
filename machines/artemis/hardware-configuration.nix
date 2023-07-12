fdb8cf6f6d3bcca9d8f6f6727268fcf22487c34e
Tobias Schmalz <github@choreutes.de>
2023-07-12 16:29:14 +0200

    Version bump and hardware configuration update due to reinstallation on artemis

--- machines/artemis/hardware-configuration.nix
+++ machines/artemis/hardware-configuration.nix
@@ -8,13 +8,13 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "ehci_pci" "xhci_pci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
-  boot.initrd.kernelModules = [ "amdgpu" ];
+  boot.initrd.availableKernelModules = [ "nvme" "ehci_pci" "xhci_pci" "usbhid" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
+  boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/2e6333ac-937d-4c14-af8b-4a41926e7e9c";
+    { device = "/dev/disk/by-uuid/c0a32360-5372-4153-ba1f-107a6a628774";
       fsType = "btrfs";
     };
 
@@ -29,4 +29,15 @@
     [ { device = "/dev/disk/by-uuid/98f754f5-749d-4bf0-ac36-d826c396b42b"; }
     ];
 
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp2s0f0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp6s0f4u1u1.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp3s0.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
