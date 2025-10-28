43b1ff1b9b7e64a8ade9db705734a70be3150768
Tobias Schmalz <github@choreutes.de>
2025-10-29 00:45:39 +0100

    Add markdown editor and games for KDE

--- home-manager/choreutes/modules/gui/kde-programs.nix
+++ home-manager/choreutes/modules/gui/kde-programs.nix
@@ -15,14 +15,18 @@ in
 
   config = lib.mkIf cfg.kde-programs.enable {
     home.packages = with pkgs.kdePackages; [
-      kaddressbook
-      kcalc
-      kclock
-      kontact
-      korganizer
-      kmail
-      plasma-nm
-      wacomtablet
+      ghostwriter   # markdown editor/previewer
+      kaddressbook  # address book
+      kcalc         # calculator
+      kclock        # (alarm) clock
+      kmines        # minesweeper
+      kontact       # KDE PIM
+      korganizer    # calendar
+      kmail         # e-mail client
+      kpat          # card game
+      ksudoku       # sudoku
+      plasma-nm     # NetworkManager applet
+      wacomtablet   # KDE settings for touch screens
     ];
 
     services.kdeconnect = {
