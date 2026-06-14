aee2832ad20be156da42ab1fddf46805f7b45f84
Tobias Schmalz <github@choreutes.de>
2026-06-14 19:39:39 +0200

    Adjust neovim config to new init.lua format
    I learned of the nixvim project shortly after making this change. I will
    probably adjust the config again using nixvim and keep this commit only
    for completeness reasons.

--- home-manager/choreutes/modules/common/neovim/default.nix
+++ home-manager/choreutes/modules/common/neovim/default.nix
@@ -4,16 +4,71 @@
   programs.neovim = {
     enable = true;
 
+    initLua = ''
+      vim.opt.number = true
+
+      vim.opt.list = true
+      vim.opt.listchars = { nbsp = '⍽', tab = '⪫ ', trail = '•' }
+
+      -- wrap is enabled by default
+      vim.opt.breakindent = true
+      vim.opt.linebreak = true
+
+      vim.opt.scrolloff = 3
+
+      -- hlsearch is enabled by default, so I don't need it here
+      vim.opt.ignorecase = true
+      vim.opt.smartcase = true
+
+      vim.opt.splitright = true
+      vim.opt.splitbelow = true
+
+      -- Setting these is apparently required for the colorscheme to persist...
+      vim.opt.background = dark
+      vim.opt.termguicolors = true
+
+      vim.cmd([[colorscheme PaperColor]])
+    '';
+
     plugins = with pkgs.vimPlugins; [
       papercolor-theme
-      vimtex
-      vim-airline
       vim-airline-themes
-      vim-ledger
+      {
+        plugin = vim-airline;
+        config = ''
+          vim.g.airline_powerline_fonts = 1
+          vim.g.airline_theme = "papercolor"
+        '';
+        type = "lua";
+      }
+      {
+        plugin = vimtex;
+        runtime = {
+          "after/ftplugin/tex.lua".text = ''
+            vim.opt.expandtab = true
+            vim.opt.shiftwidth = 2
+            vim.opt.softtabstop = 2
+          '';
+        };
+        type = "lua";
+      }
+      {
+        plugin = vim-ledger;
+        runtime = {
+          "after/ftplugin/ledger.lua".text = ''
+            vim.opt.shiftwidth = 4
+            vim.opt.tabstop = 4
+
+            vim.opt.foldenable = true
+            vim.opt.foldmethod = "syntax"
+          '';
+        };
+        type = "lua";
+      }
       vim-nix
     ];
 
-    sideloadInitLua = true;
+    # sideloadInitLua = true;
 
     # Disabling these is the new default starting with 26.05.
     # I am not aware of any plugins actually using these
@@ -22,7 +77,7 @@
     withRuby = false;
   };
 
-  xdg.configFile."nvim" = {
-    source = ./config-files;
-  };
+  # xdg.configFile."nvim" = {
+  #   source = ./config-files;
+  # };
 }
